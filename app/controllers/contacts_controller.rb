2c8f2c0c3b7dad056686ce3016f7d2fa2df91215
Lowell Mower <lowell.mower@gmail.com>
2015-02-09 10:56:36 -0500

    styled out remaining pages changed all forms to ruby forms and added title tags

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,41 +1,39 @@
 class ContactsController < ApplicationController
 	def index
 		@contacts = Contact.all
-		render('contacts/index.html.erb')
 	end
 
 	def new
 		@contact = Contact.new
-		render('contacts/new.html.erb')
 	end
 
 	def create
 		@contact = Contact.new(params[:contact])
 		if @contact.save
 			flash[:notice] = "Your contact was saved to Wikipages!"
-			redirect_to("/contacts/#{@contact.id}")
+			redirect_to contacts_path
+			#redirect_to("/contacts/#{@contact.id}")
 		else
-			render('contacts/new.html.erb')
+			render 'new'
 		end
 	end
 
 	def show
 		@contact = Contact.find(params[:id])
-		render('contacts/show.html.erb')
 	end
 
 	def edit
 		@contact = Contact.find(params[:id])
-		render('contacts/edit.html.erb')
 	end
 
 	def update
 		@contact = Contact.find(params[:id])
 			if @contact.update(params[:contact])
 				flash[:notice] = "Contact was successfully update!"
-				redirect_to("/contacts/#{@contact.id}")
+				redirect_to contacts_path(@contact)
+				#redirect_to("/contacts/#{@contact.id}")
 			else
-				render('contacts/edit.html.erb')
+				render 'edit'
 		end
 	end
 
@@ -43,6 +41,6 @@ class ContactsController < ApplicationController
 		@contact = Contact.find(params[:id])
 		@contact.destroy
 		flash[:notice] = "The contact: '#{@contact.name}' was deleted!"
-		redirect_to('/contacts')
+		redirect_to contacts_path
 	end
 end
--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -2,32 +2,29 @@ class PhonesController < ApplicationController
 	def new
 		contact = Contact.find(params[:contact_id])
 		@phone = contact.phones.new
-		render('phones/new.html.erb')
 	end
 
 	def create
 		@phone = Phone.new(:number => params[:number],
 											:contact_id => params[:contact_id])
 		if @phone.save
-			render('phones/success.html.erb')
+			render 'success'
 		else
-			render('phones/new.html.erb')
+			render 'new'
 		end
 	end
 
 	def destroy
-		@phone = Phone.find(params[:phone_id])
+		@phone = Phone.find(params[:id])
 		@phone.destroy
-		render('phones/destroy.html.erb')
 	end
 
 	def edit
-		@phone = Phone.find(params[:phone_id])
-		render('phones/edit.html.erb')
+		@phone = Phone.find(params[:id])
 	end
 
 	def update
-		@phone = Phone.find(params[:phone_id])
+		@phone = Phone.find(params[:id])
 		@phone.update(params[:phone])
 		render('phones/success.html.erb')
 	end
